e6f895e5b964e61e266ad37bbb68520324be82fa
Everett Smith <evepsmith15@gmail.com>
2019-07-10 23:37:40 -0700

    more code added

--- db/schema.sql
+++ db/schema.sql
@@ -3,7 +3,7 @@
 CREATE DATABASE burger_db;
 USE burger_db;
 
-CREATE TABLE cats
+CREATE TABLE burgers
 (
 	id int NOT NULL AUTO_INCREMENT,
 	burger_name varchar(50) NOT NULL,
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Chesseburger');
+INSERT INTO burgers (burger_name) VALUES ('Hamburger');
+INSERT INTO burgers (burger_name, devour) VALUES ('Quarter Pounder', true);
+INSERT INTO burgers (burger_name, devour) VALUES ('Royal Red Robin', true);
+INSERT INTO burgers (burger_name, devour) VALUES ('Banzai Burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
