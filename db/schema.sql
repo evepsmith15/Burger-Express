f1566a73a6b4ff783c58c71ae576ffd1a4c9d304
Everett Smith <evepsmith15@gmail.com>
2019-07-10 23:23:54 -0700

    deleted js folder

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+### Schema
+
+CREATE DATABASE burger_db;
+USE burger_db;
+
+CREATE TABLE cats
+(
+	id int NOT NULL AUTO_INCREMENT,
+	burger_name varchar(50) NOT NULL,
+	devour BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
+);
